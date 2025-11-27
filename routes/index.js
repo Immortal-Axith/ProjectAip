ec1535a9ab97c1657aee4aff48ed009585bf3232
Эбазер Типпа <axith@MacBook-Air-Ebazer-1337.local>
2025-11-27 12:38:51 +0300

    Пункт_4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Рубля */
+router.get('/rub', function(req, res, next) {
+    res.send("<h1>Страница Рубля</h1>")
+});
+
+/* Страница Доллара */
+router.get('/usd', function(req, res, next) {
+    res.send("<h1>Страница Даллара</h1>")
+});
+
+/* Страница Евро */
+router.get('/eu', function(req, res, next) {
+    res.send("<h1>Страница Евро</h1>")
+});
+
 module.exports = router;
